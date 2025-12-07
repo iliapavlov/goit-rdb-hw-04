95bc38f15b71d99acffc645224588bb1342a3e33
Ilya <elijapavlov@gmail.com>
2025-12-07 23:27:54 +0200

    task 4.2 completed;

--- hw4.sql
+++ hw4.sql
@@ -47,3 +47,29 @@ CREATE TABLE IF NOT EXISTS borrowed_books (
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+-- task-2 Заповніть таблиці простими видуманими тестовими даними
+INSERT INTO authors(author_name) 
+VALUES ('Jean Louis De Lolme'), ('Josephine Quintero'), ('Rosemary Kirstein');
+
+INSERT INTO genres(genre_name)
+VALUES ('historic'), ('cultural'), ('novel');
+
+INSERT INTO books(title, publication_year, author_id, genre_id)
+VALUES ("Constitution de l'Angleterre", 1971, 1, 1),
+	   ("Dubaï en quelques jours", 2012, 2, 2),
+       ("The Steerswoman", 1989, 3, 3);
+
+INSERT INTO users(username, email)
+VALUES 
+    ('Олександр Петренко', 'alex.p@example.com'),
+    ('Марія Сидоренко', 'maria.sydor@test.net'),
+    ('Іван Коваль', 'ivan.koval@mail.com');
+    
+INSERT INTO borrowed_books(book_id, user_id, borrow_date, return_date)
+VALUES 
+    (1, 1, '2025-10-10', '2025-10-24'),
+    (2, 2, '2025-11-05', NULL),
+    (3, 1, '2025-11-12', '2025-11-20');
+
+
